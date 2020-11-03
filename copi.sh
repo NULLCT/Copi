b82dc4b1bca56552451e7cbdb756484f214df7bc
NULLCT <cockatiel.u10@gmail.com>
2020-11-03 14:02:41 +0900

    switch sentence from if excutable...

--- copi.sh
+++ copi.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-stdin=`cat`
+#     ____            _    #
+#    / ___|___  _ __ (_)   #
+#   | |   / _ \| '_ \| |   #
+#   | |__| (_) | |_) | |   #
+#    \____\___/| .__/|_|   #
+#              |_|         #
+#      Made by NULLCT      #
 
-if [ "$(uname)" = 'Darwin' ]; then #Mac
-  echo $stdin | pbcopy
-else #Linux
-  echo $stdin | xclip -in -sel clip
+if type "pbcopy" > /dev/null 2>&1; then # pbcopy
+  'cat' | pbcopy
+  echo "Copy by pbcopy"
+else
+  if type "xclip" > /dev/null 2>&1; then # pbcopy
+    'cat' | xclip -in -sel clip
+    echo "Copy by xclip"
+  else
+    echo "Not found command for copy" >&2 # 404
+  fi
 fi
